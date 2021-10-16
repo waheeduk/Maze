854dfd79969bdc519969acbd3e77189e8ba7fcb3
Mohammed Ahmed <waheedahmed14@gmail.com>
2021-10-16 23:10:08 +0100

    created a way to visualise generated mazes

--- maze.py
+++ maze.py
@@ -1,17 +1,23 @@
 import random
 
-maze_width = 4
-maze_height = 4
+#size of the maze
+maze_width = 20
+maze_height = 20
 
+#holds all tiles we visit as we go across
 path = [ ]
+#holds the latest tiles we've visited in order
 stack = [ ]
 
+#class to hold vector2 coordinates with added cosntructors
 class Vector2():
     def __init__(self, x, y):
         self.x = x
         self.y = y
     def __add__(self, other):
         return Vector2(self.x + other.x, self.y + other.y)
+    def __sub__(self, other):
+        return Vector2(self.x - other.x, self.y - other.y)
     def __eq__(self, other):
         if isinstance(other, Vector2):
             return self.x == other.x and self.y == other.y
@@ -29,34 +35,43 @@ directions.append(down)
 directions.append(left)
 directions.append(right)
 
+#variable to count number of total tiles we've visited
 filled = 1
+#starts at top left
 start_pos = Vector2(0, 0)
 
 def generate_maze(position, width, height, filled):
+    """function that generates recursively generates a maze that visits 
+    every tile in a grid"""
     if position not in stack:
         stack.append(position)
     path.append(position)
-    if filled == 25:
+    if filled == (maze_width+1)*(maze_height+1):
         return 
     else:
+        #holds possible routes for maze to travel
         options = [ ]
         for dirs in directions:
+            #generates directions in four cardinals
             result = Vector2(0, 0)
             result = position + dirs
+            #checks out of bounds
             if result.x < 0 or result.x > width or result.y < 0 or \
                 result.y > height:
                 pass
+            #prevents doubling back
             elif result in path:
                 pass
             else:
                 options.append(result)
+        #checks if we've backed into a corner, in which case, we go backtrack
         if len(options) == 0:
             if len(stack) != 1:
                 stack.pop()
-                print(f"stack popped new gen maze with pos of {stack[-1]}")
                 return generate_maze(stack[-1], width, height, filled)
             else:
                 return generate_maze(stack[-1], width, height, filled)
+        #saves a viable route
         else:
             new_pos = random.choice(options)
             filled += 1
--- visual.py
+++ visual.py
@@ -2,14 +2,17 @@ import pygame
 import sys
 from maze import *
 
-maze_tile_size = 20
-black = (0, 0, 0)
-white = (255, 255, 255)
+#variables to hold information about rendered game
+maze_tile_size = 10
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+screen_width = (int(maze_tile_size * 1.5) * maze_width)
+screen_height = (int(maze_tile_size * 1.5) * maze_height)
 
 pygame.init()
-screen=pygame.display.set_mode((maze_width*maze_tile_size, maze_height*maze_tile_size))
+screen=pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Maze')
-screen.fill((50,255,255))
+screen.fill(BLACK)
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT: #exit
@@ -18,5 +21,22 @@ while True:
     pygame.display.update() #UPDATE DRAW FUNCTION
     #draws tiles in path
     for i in range(len(path)):
-        pygame.draw.rect(screen, white, pygame.Rect(path[i].x*maze_tile_size, path[i].y*maze_tile_size, maze_tile_size, maze_tile_size), 0)
-    # pygame.draw.rect(screen, white, pygame.Rect(0, 0, maze_tile_size, maze_tile_size), 0)
+        tile_pos = Vector2((path[i].x*maze_tile_size)+ ((maze_tile_size/2)*path[i].x), (path[i].y*maze_tile_size)+((maze_tile_size/2)*path[i].y))
+        pygame.draw.rect(screen, WHITE, pygame.Rect(tile_pos.x, tile_pos.y, maze_tile_size , maze_tile_size), 0)
+        #draws walls or lack thereof between tiles
+        if i <= len(path)-2:
+            j = path[i+1]-path[i]
+            if j == up:
+                wall_pos = tile_pos - Vector2(0, maze_tile_size/2)
+                pygame.draw.rect(screen, WHITE, pygame.Rect(wall_pos.x, wall_pos.y, maze_tile_size, maze_tile_size/2), 0)
+            if j == down:
+                wall_pos = tile_pos + Vector2(0, maze_tile_size)
+                pygame.draw.rect(screen, WHITE, pygame.Rect(wall_pos.x, wall_pos.y, maze_tile_size, maze_tile_size/2), 0)
+            if j == left:
+                wall_pos = tile_pos - Vector2(maze_tile_size/2, 0)
+                pygame.draw.rect(screen, WHITE, pygame.Rect(wall_pos.x, wall_pos.y, maze_tile_size/2, maze_tile_size), 0)
+            if j == right:
+                wall_pos = tile_pos + Vector2(maze_tile_size, 0)
+                pygame.draw.rect(screen, WHITE, pygame.Rect(wall_pos.x, wall_pos.y, maze_tile_size/2, maze_tile_size), 0)
+    pygame.image.save(screen, "media/maze_example.png")
+            
